892d36b322419d541d4396ba4bf7c07ad83e326a
Jonathan <jonathan.lucht@stud.th-luebeck.de>
2024-12-07 15:02:02 +0100

    added new text animation & sidebar

--- js/index.js
+++ js/index.js
@@ -8,3 +8,16 @@ themeToggle.addEventListener('change', () => {
     document.body.classList.toggle('dark-theme');
     document.body.classList.toggle('light-theme');
 });
+
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    const body = document.body;
+
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+        body.classList.remove("sidebar-open");
+    } else {
+        sidebar.style.width = "250px";
+        body.classList.add("sidebar-open");
+    }
+}
