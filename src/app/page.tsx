07733dabdca7b7dca595952e03258f34ab8983f0
Jonathan Lucht <jonathan.lucht@stud.th-luebeck.de>
2025-10-21 14:11:36 +0200

    added more text and css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import "./styles/globals.css";
+import "./styles/home.css";
 
 export const metadata = {
   title: "LuchtDev: Homepage",
   description: "",
 };
 
-
 export default function Home() {
   return (
     <main>
       <div className="wrapper">
-        <h1>Willkommen!</h1>
-        Wie geht es dir?
+        <h1>Hallo!</h1>
+        <h1>
+          Ich bin <div className="elevated">J</div>onathan,
+        </h1>
+        <h1>Full-Stack Developer</h1>
       </div>
     </main>
   );
